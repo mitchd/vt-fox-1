411e279b61ae6e354e445ca83bb39bfaedf0354a
Mitch Davis <mitchd@vt.edu>
2012-12-31 16:14:36 -0500

    Working on the SPI flash interface.
    Got most of the byte and AAI write sequences completed.
    
    -MD

--- stm32f4-devel/spi_flash/spi_flash.c
+++ stm32f4-devel/spi_flash/spi_flash.c
@@ -1,1 +1,136 @@
 #include "spi_flash.h"
+
+static uint8_t flash_addr[3];
+static uint8_t flash_cmd;
+
+void configureSPI_Flash(void){
+  //Initialize the SPI port
+  spiStart(&SPID1, &spi1cfg);
+
+  //Setup the pads
+  
+  //Configure NSS
+  palSetPad(GPIOA, 3);
+  palSetPadMode( GPIOA, 3, PAL_MODE_OUTPUT_PUSHPULL |
+                           PAL_STM32_OSPEED_HIGHEST );
+
+  //Configure SCK
+  palSetPadMode( GPIOA, 5, PAL_MODE_ALTERNATE(5) |
+                           PAL_STM32_OSPEED_HIGHEST );
+
+  //Configure MISO
+  palSetPadMode( GPIOA, 6, PAL_MODE_ALTERNATE(5) );
+
+  //Configure MOSI
+  palSetPadMode( GPIOA, 7, PAL_MODE_ALTERNATE(5) |
+                           PAL_STM32_OSPEED_HIGHEST );
+}
+
+
+void flashWriteByte( uint32_t addr, uint8_t data ){
+  //Ensure that we get a legitimate address
+  addr &= FLASH_HIGH_ADDR-1;
+  //Map addr into 8-bit words to send
+  flash_addr[0] = (addr & 0xFF0000)>>16; //MSB
+  flash_addr[1] = (addr & 0x00FF00)>>8;
+  flash_addr[2] = (addr & 0x0000FF);     //LSB
+  //Set the command
+  flash_cmd = FLASH_WRITE_ENABLE;
+  //Acquire the SPI device
+  spiAcquireBus( &SPID1 );
+  //Prepare for data transfer
+  spiSelect( &SPID1 );
+  //Transmit the write enable command
+  spiSend( &SPID1, 1, &flash_cmd );
+  //Transmit the byte write command
+  flash_cmd = FLASH_BYTE_PROGRAM;
+  spiSend( &SPID1, 1, &flash_cmd );
+  //Transmit the destination address
+  spiSend( &SPID1, 3, flash_addr );
+  //Transmit the byte
+  spiSend( &SPID1, 1, &data );
+  //Transmit the write disable command
+  flash_cmd = FLASH_WRITE_DISABLE;
+  spiSend( &SPID1, 1, &flash_cmd );
+  //End the transfer
+  spiUnselect( &SPID1 );
+  //Release the bus
+  spiReleaseBus( &SPID1 );
+
+  return;
+}
+
+void flashWriteBytes( uint32_t addr, uint8_t* data, uint32_t n ){
+  const uint8_t zero_fill = 0x00;
+  uint32_t tx_bytes = 2;
+  uint8_t  flash_status = 0;
+  //Ensure that we get a legitimate address
+  addr &= FLASH_HIGH_ADDR-1;
+  //Prevent overflow
+  if( n > (FLASH_HIGH_ADDR - addr) )
+    return;
+  //Map addr into 8-bit words to send
+  flash_addr[0] = (addr & 0xFF0000)>>16; //MSB
+  flash_addr[1] = (addr & 0x00FF00)>>8;
+  flash_addr[2] = (addr & 0x0000FF);     //LSB
+  //Software EndOfWrite Detection
+  flash_cmd = FLASH_DBSY;
+  //Acquire the SPI device
+  spiAcquireBus( &SPID1 );
+  //Prepare for data transfer
+  spiSelect( &SPID1 );
+  //Transmit software EOW Detection
+  spiSend( &SPID1, 1, &flash_cmd );
+  //Set the WEN command
+  flash_cmd = FLASH_WRITE_ENABLE;
+  //Transmit the write enable command
+  spiSend( &SPID1, 1, &flash_cmd );
+  //AAI Command
+  flash_cmd = FLASH_AAI_PROGRAM
+  //Transmit the AAI command
+  spiSend( &SPID1, 1, &flash_cmd );
+  //Transmit the destination address
+  spiSend( &SPID1, 3, flash_addr );
+  //Transmit the first two bytes
+  spiSend( &SPID1, 2, &data );
+  //Send NSS High
+  spiUnselect( &SPID1 );
+  //Loop through the rest of data
+  while( tx_bytes < n ){
+    //Send NSS Low
+    spiSelect( &SPID1 );
+    //Set the read status register command
+    flash_cmd = FLASH_READ_STATUS;
+    spiSend( &SPID1, 1, &flash_cmd );
+    //Get the status
+    spiReceive( &SPID1, 1, &flash_status );
+    if( ~(FLASH_STATUS_BUSF & flash_status) ){  //Check for write complete
+      //Set the AAI command
+      flash_cmd = FLASH_AAI_PROGRAM;
+      //Transmit the AAI command
+      spiSend( &SPID1, 1, &flash_cmd );
+      if( tx_bytes+1 == n ){ //Check to see if we need a fill
+        //Transmit the last byte
+        spiSend( &SPID1, 1, data+tx_bytes );
+        //Transmit the fill
+        spiSend( &SPID1, 1, &zero_fill );
+        tx_bytes++;
+      }else{
+        spiSend( &SPID1, 2, data+tx_bytes );
+        tx_bytes += 2;
+      }
+    }
+    //Send NSS High
+    spiUnselect( &SPID1 );
+  }
+  //Send NSS Low
+  spiSelect( &SPID1 );
+  //Set write disable
+  flash_cmd = FLASH_WRITE_DISABLE;
+  spiSend( &SPID1, 1, &flash_cmd );
+  //Send NSS high
+  spiUnselect( &SPID1 );
+  //Release the bus
+  spiReleaseBus( &SPID1 );
+  return;
+}
--- stm32f4-devel/spi_flash/spi_flash.h
+++ stm32f4-devel/spi_flash/spi_flash.h
@@ -2,8 +2,12 @@
 
 #define __SPI_FLASH__
 
+#include "hal.h"
+
 //Opcodes for Microchip SST Flash
 //Currently for their 8Mb module
+#define FLASH_LOW_ADDR			0x000000
+#define FLASH_HIGH_ADDR			0x800000
 #define FLASH_READ 			0x03
 #define FLASH_HS_READ 			0x0B
 #define FLASH_4KB_ERASE			0x20
@@ -33,7 +37,19 @@
 #define FLASH_STATUS_AAI		0x40  //1 = AAI mode 0 = Byte-program
 #define FLASH_STATUS_BPL		0x80  //1 = BPx are read-only
 
+/*SPI Master
+ * On the STM32F4 board this should run at ~21MHz
+ */
+const SPIConfig spi1cfg = {
+  NULL, 	//Callback function
+  GPIOA, 	//NSS Port
+  3,		//NSS Pad
+  NULL
+};
 
-
-
+void 	configureSPI_Flash(void);
+void 	flashWriteByte( uint32_t addr, uint8_t data );
+uint8_t flashReadByte( uint32_t addr );
+void	flashWriteBytes( uint32_t addr, uint8_t* data, uint32_t n );
+void	flashReadBytes( uint32_t addr, uint8_t* data, uint32_t n );
 #endif
