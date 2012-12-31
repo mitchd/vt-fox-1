7ca8f69a18bc5ed6041e4a92d0a39b73eff3085a
Mitch Davis <mitchd@vt.edu>
2012-12-31 11:12:02 -0500

    Beginning work on the spi flash module
    -MD

---
+++ stm32f4-devel/spi_flash/spi_flash.h
@@ -0,0 +1,39 @@
+#ifndef __SPI_FLASH__
+
+#define __SPI_FLASH__
+
+//Opcodes for Microchip SST Flash
+//Currently for their 8Mb module
+#define FLASH_READ 			0x03
+#define FLASH_HS_READ 			0x0B
+#define FLASH_4KB_ERASE			0x20
+#define FLASH_32KB_ERASE		0x52
+#define FLASH_64KB_ERASE		0xD8
+#define FLASH_CHIP_ERASE		0x60
+#define FLASH_BYTE_PROGRAM		0x02
+#define FLASH_AAI_PROGRAM		0xAD
+#define FLASH_READ_STATUS		0x05
+#define FLASH_ENABLE_WRITE_STATUS	0x50
+#define FLASH_WRITE_STATUS		0x01
+#define FLASH_WRITE_ENABLE		0x06
+#define FLASH_WRITE_DISABLE		0x04
+#define FLASH_READ_ID			0x90
+#define FLASH_READ_JDEC_ID		0x9F
+#define FLASH_EBSY			0x70
+#define FLASH_DBSY			0x80
+
+
+//Status Register Values
+#define FLASH_STATUS_BUSY		0x01  //1 = write in progress
+#define FLASH_STATUS_WEL		0x02  //1 = device write enabled
+#define FLASH_STATUS_BP0		0x04  //------
+#define FLASH_STATUS_BP1		0x08  //Indicate current level of
+#define FLASH_STATUS_BP2		0x10  //block protection
+#define FLASH_STATUS_BP3		0x20  //------
+#define FLASH_STATUS_AAI		0x40  //1 = AAI mode 0 = Byte-program
+#define FLASH_STATUS_BPL		0x80  //1 = BPx are read-only
+
+
+
+
+#endif
---
+++ stm32f4-devel/spi_flash/spi_flash.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+SPIFLASHSRC = ${CHIBIOS}/spi_flash/spi_flash.c
+
+# Required include directories
+SPIFLASHINC = ${CHIBIOS}/spi_flash
