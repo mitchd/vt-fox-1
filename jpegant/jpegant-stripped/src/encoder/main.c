mitchd/vt-fox-1

commit e9d0756a498c00413eef874d41d0565a020b81f1

I uh... messed up some things.  Fixlater.
-MD

jpegant/jpegant-stripped/src/encoder/main.c
@@ -34,15 +34,15 @@ inline color RGB2Cr(const color r, const color g, const color b)
 // chroma subsampling, i.e. converting a 16x16 RGB block into 8x8 Cb and Cr
 void subsample(RGB rgb[8][16], short cb[8][8], short cr[8][8])
 {
-	RGB pixel;
+	color red,blue,green;	
 	for (unsigned r = 0; r < 8; r++)
 	for (unsigned c = 0; c < 8; c++)
 	{
-		pixel.Red = (rgb[r][2*c].Red+rgb[r][2*c+1].Red)/2;
-		pixel.Green = (rgb[r][2*c].Green+rgb[r][2*c+1].Green)/2;
-		pixel.Blue = (rgb[r][2*c].Blue+rgb[r][2*c+1].Blue)/2;
-		cb[r][c] = (short)RGB2Cb( pixel.Red, pixel.Green, pixel.Blue )-128;
-		cr[r][c] = (short)RGB2Cr( pixel.Red, pixel.Green, pixel.Blue )-128;
+		red = (getRed(rgb[r][2*c])+getRed(rgb[r][2*c+1]))/2;
+		green = (getGreen(rgb[r][2*c])+getGreen(rgb[r][2*c+1]))/2;
+		blue = (getBlue(rgb[r][2*c])+getBlue(rgb[r][2*c+1]))/2;
+		cb[r][c] = (short)RGB2Cb( red, green, blue )-128;
+		cr[r][c] = (short)RGB2Cr( red, green, blue )-128;
 	}
 }
 
@@ -90,14 +90,14 @@ int main (int argc, char *argv[])
 			for (unsigned r = 0; r < 8; r++)
 			for (unsigned c = 0; c < 8; c++)
 			{
-				color R = RGB8x16[r][c].Red;
-				color G = RGB8x16[r][c].Green;
-				color B = RGB8x16[r][c].Blue;
+				color R = getRed(RGB8x16[r][c]);
+				color G = getGreen(RGB8x16[r][c]);
+				color B = getBlue(RGB8x16[r][c]);
 				// converting RGB into Y (luminance)
 				Y8x8[0][r][c] = RGB2Y(R, G, B)-128;
-				R = RGB8x16[r][c+8].Red;
-				G = RGB8x16[r][c+8].Green;
-				B = RGB8x16[r][c+8].Blue;
+				R = getRed(RGB8x16[r][c+8]);
+				G = getGreen(RGB8x16[r][c+8]);
+				B = getBlue(RGB8x16[r][c+8]);
 				// converting RGB into Y (luminance)
 				Y8x8[1][r][c] = RGB2Y(R, G, B)-128;
 			}

jpegant/jpegant-stripped/src/encoder/rgbdata.c
@@ -52,3 +52,19 @@ bool load_rgb( char *filename, RGB **mmap_addr )
 
 	return true;
 }
+
+color getRed( RGB values ){
+  color red = (values.A & 0xF8);
+  return red;
+}
+
+color getBlue( RGB values ){
+  color blue = (values.B & 0x1F)<<3;
+  return blue;
+}
+
+color getGreen( RGB values ){
+  color green = (values.A & 0x07) << 5;
+  green |= (values.B & 0xE0) >> 5;
+  return green;
+}

jpegant/jpegant-stripped/src/encoder/rgbdata.h
@@ -35,9 +35,8 @@ typedef unsigned char color;
 
 
 typedef struct {
-	color Red;
-	color Green;
-	color Blue;
+        color A;
+        color B;
 } RGB;
 
 /*
@@ -46,6 +45,9 @@ typedef struct {
 
 #ifdef __X86
 
+color getRed( RGB values );
+color getBlue( RGB values );
+color getGreen( RGB values );
 /*
  * Load the specified filename into a memory map, pointed at by mmap_addr
  *

jpegant/jpegant-stripped-yuv/src/encoder/main.c
@@ -1,0 +1,101 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <math.h>
+#include <errno.h>
+#include "rgbdata.h"
+#include "dct.h"
+#include "jpegenc.h"
+
+int file_jpg;
+
+void write_jpeg(const unsigned char buff[], const unsigned size)
+{
+	write(file_jpg, buff, size);
+}
+
+// chroma subsampling, i.e. converting a 16x16 RGB block into 8x8 Cb and Cr
+void subsample(YUV yuv[8][16], short cb[8][8], short cr[8][8])
+{
+	for (unsigned r = 0; r < 8; r++)
+	for (unsigned c = 0; c < 16; c+=2)
+	{
+		cb[r][c/2] = yuv[r][c].UV-128;
+		cr[r][c/2] = yuv[r][c+1].UV-128;
+	}
+}
+
+
+int main (int argc, char *argv[])
+{
+	if (argc < 3) {
+		fprintf(stderr, "Usage: %s file-in.bmp file-out.jpg\n", argv[0]);
+		return -1;
+	}
+
+	YUV *mmap_addr = NULL;
+
+	if (!load_yuv(argv[1],&mmap_addr)) {
+		fprintf(stderr, "Error: cannot open %s\n", argv[1]);
+		return -1;
+	}
+
+	short Y8x8[2][8][8]; // four 8x8 blocks - 16x16
+	short Cb8x8[8][8];
+	short Cr8x8[8][8];
+        YUV   YUV8x16[8][16];
+
+	if ((file_jpg = open(argv[2], O_CREAT|O_TRUNC|O_WRONLY,S_IRUSR|S_IWUSR )) < 0) {
+		fprintf(stderr, "Error: cannot create %s (%i)\n", argv[2], errno);
+		return -1;
+	}
+
+	// Process image by 8x16 blocks
+	// The resulting image will be truncated on the right/down side
+	// if its width/height is not N*8.
+	// The data is written into <file_jpg> file by write_jpeg() function
+	// which Huffman encoder uses to flush its output, so this file
+	// should be opened before the call of huffman_start().
+	huffman_start(IMG_HEIGHT & -8, IMG_WIDTH & -8);
+	for (unsigned y = 0; y < IMG_HEIGHT; y += 8) {
+		for (unsigned x = 0; x < IMG_WIDTH; x += 16)
+		{
+			if (!yuv_get_block(x, y, 16, 8, (YUV*)YUV8x16,
+				mmap_addr)) {
+				printf("Error: getBlock(%d,%d)\n", x, y);
+				break;
+			}
+
+			for (unsigned r = 0; r < 8; r++)
+			for (unsigned c = 0; c < 8; c++)
+			{
+				Y8x8[0][r][c] = YUV8x16[r][c].Y-128;
+				Y8x8[1][r][c] = YUV8x16[r][c+8].Y-128;
+			}
+		
+			//getting Cb and Cr
+			subsample(YUV8x16, Cb8x8, Cr8x8);
+
+			// 1 Y-compression
+			dct(Y8x8[0], Y8x8[0]);
+			huffman_encode(HUFFMAN_CTX_Y, (short*)Y8x8[0]);
+			// 2 Y-compression
+			dct(Y8x8[1], Y8x8[1]);
+                        huffman_encode(HUFFMAN_CTX_Y, (short*)Y8x8[1]);
+			// 1 Cb-compression
+			dct(Cb8x8, Cb8x8);
+			huffman_encode(HUFFMAN_CTX_Cb, (short*)Cb8x8);
+			// 1 Cr-compression
+			dct(Cr8x8, Cr8x8);
+			huffman_encode(HUFFMAN_CTX_Cr, (short*)Cr8x8);
+		}
+	}
+
+	huffman_stop();
+	close(file_jpg);
+	close_yuv(argv[1],mmap_addr);
+	return 0;
+}

jpegant/jpegant-stripped-yuv/src/encoder/rgbdata.c
@@ -1,0 +1,54 @@
+#include "rgbdata.h"
+
+bool yuv_get_block(unsigned x, unsigned y, unsigned sx, unsigned sy, 
+			YUV *buf, YUV *addr)
+{
+	if ((y + sy) > IMG_HEIGHT || (x + sx) > IMG_WIDTH) {
+        	return false;
+        }
+
+        for (unsigned r = 0; r < sy; r++) {
+                unsigned offset = (y+r)*IMG_WIDTH + x;
+                for (unsigned c = 0; c < sx; c++) {
+	                unsigned i = offset + c;
+                        buf[sx*r + c] = addr[i];
+                }
+        }
+
+        return true;
+}
+
+void close_yuv( char *filename, YUV *mmap_addr )
+{
+	struct stat sbuf;
+
+	if(stat(filename,&sbuf) == -1){
+                return;
+        }
+
+	munmap( mmap_addr, sbuf.st_size );
+	return;
+}
+
+bool load_yuv( char *filename, YUV **mmap_addr )
+{
+	int fd;
+	struct stat sbuf;
+
+	if((fd = open(filename,O_RDONLY)) == -1){
+		return false;
+	}
+
+	if(stat(filename,&sbuf) == -1){
+		return false;
+	}
+
+	*mmap_addr = mmap(0, sbuf.st_size, PROT_READ, MAP_PRIVATE, 
+			fd, 0);
+	
+	if( *mmap_addr == (void*)-1){
+		return false;
+	}
+
+	return true;
+}

jpegant/jpegant-stripped-yuv/src/encoder/rgbdata.h
@@ -1,0 +1,82 @@
+/*
+ * rgbdata.h
+ *
+ * This header provides functionality for jpegant to access RGB pixels from raw
+ * RGB data.
+ * On the x86 platform we do this with a memory-mapped file
+ *
+ * For the ARM platform, the functions request data from SPI Flash
+ *
+ * This functionality is **very specific** to a 640x480 pixel image, as this 
+ * code is purpose built for AMSAT FOX-1
+ *
+ * Mitch Davis
+ * Virginia Tech, 2012
+ * mitchd@vt.edu
+ */
+
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __X86
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#endif
+
+/*
+ * Define our image sizes here
+ */
+#define IMG_WIDTH 320
+#define IMG_HEIGHT 240
+
+typedef uint8_t color;
+
+
+typedef struct {
+	color Y;
+	color UV;
+} YUV;
+
+/*
+ * Compile with -D__X86 to build with memory map functions
+ */
+
+#ifdef __X86
+
+/*
+ * Load the specified filename into a memory map, pointed at by mmap_addr
+ *
+ * filename	: name of the YUV file with IMG_WIDTH x IMG_HEIGHT RGB values
+ * mmap_addr	: address of the mmap, generated by the OS with an mmap call
+ */
+bool load_yuv( char *filename, YUV **mmap_addr );
+
+void close_yuv( char *filename, YUV *mmap_addr );
+
+/*
+ * addr points to an IMG_WIDTH x IMG_HEIGHT YUV 4-2-2 array (such as one from 
+ * load_rgb)
+ *
+ * This then gets a small rectangle from the image, starting at coordinates 
+ * (x,y)
+ * The rectangle is of size (sx,sy)
+ *
+ * Data is entered into buf.
+ *
+ * It is assumbed that buf is of sufficient size to store the requested data
+ *
+ * x		: x-coordinate in pixles of the rectangle origin 
+ * (top-left corner)
+ * y		: y-coordinate in pixles of the rectangle origin 
+ * (top-left corner)
+ * sx		: width of the rectangle in pixles
+ * sy		: height of the rectangle in pixles
+ * buf		: buffer to copy the RGB data into
+ * addr		: address pointing to the origin of the RGB data
+ */
+bool yuv_get_block( unsigned x, unsigned y, unsigned sx, unsigned sy, 
+			YUV *buf, YUV *addr );
+#endif

stm32l1-devel/encoder/rgbdata.h
@@ -17,25 +17,19 @@
 
 
 #include <stdint.h>
-#include <stdbool.h>
-
-#ifdef __X86
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#endif
 
 /*
  * Define our image sizes here
  */
-#define IMG_WIDTH 640
+#define IMG_WIDTH 640 
 #define IMG_HEIGHT 480
 
 typedef uint8_t color;
 
 
 typedef struct {
-	color Red;
-	color Green;
-	color Blue;
+        color A;
+        color B;
 } RGB;
+
+

stm32l1-devel/main.c
@@ -37,13 +37,13 @@ This entire project is licensed under the GNU Public License (GPL) Version 3:
 */
 #include "ch.h"
 #include "hal.h"
-#include "uart_iface.h"
-#include "spi_flash.h"
+//#include "uart_iface.h"
+//#include "spi_flash.h"
 #include "camera_iface.h"
 #include "chprintf.h"
 
-WORKING_AREA(waUART_Thread, 512);
-WORKING_AREA(waCamera_Thread, 1280+256);
+//WORKING_AREA(waUART_Thread, 512);
+WORKING_AREA(waCamera_Thread, 10240+1024);
 
 /*
  * Application entry point.
@@ -64,9 +64,9 @@ int main(void) {
    * Activates the serial driver 3.
    * PB10(TX) and PB11(RX) are routed to USART3.
    */
-  sdStart(&SD3, NULL);
-  palSetPadMode(GPIOB, 10, PAL_MODE_ALTERNATE(7));
-  palSetPadMode(GPIOB, 11, PAL_MODE_ALTERNATE(7));
+  //sdStart(&SD3, NULL);
+  //palSetPadMode(GPIOB, 10, PAL_MODE_ALTERNATE(7));
+  //palSetPadMode(GPIOB, 11, PAL_MODE_ALTERNATE(7));
 
   /* Activate serial driver 1 for debug
    *  PA9 (TX) and PA10(RX)
@@ -79,12 +79,12 @@ int main(void) {
   /*
    * Create our UART thread
    */
-  chThdCreateStatic(waUART_Thread, sizeof(waUART_Thread), NORMALPRIO,
-                    UART_Thread,NULL);
+  //chThdCreateStatic(waUART_Thread, sizeof(waUART_Thread), NORMALPRIO,
+  //                  UART_Thread,NULL);
 
   //configureSPIFlash();
 
-  chThdCreateStatic(waCamera_Thread, sizeof(waCamera_Thread), NORMALPRIO,
+  chThdCreateStatic(waCamera_Thread, sizeof(waCamera_Thread), HIGHPRIO,
                     cameraControlThread,NULL);
   /*
    * Normal main() thread activity, in this demo it does nothing except

commit 70d38391500b78dab726b3efc1358586cbb49c90

jpegant: reverting broken files to 7d14a65b06 from e9d0756a49

jpegant/jpegant-stripped/src/encoder/main.c
@@ -34,15 +34,15 @@ inline color RGB2Cr(const color r, const color g, const color b)
 // chroma subsampling, i.e. converting a 16x16 RGB block into 8x8 Cb and Cr
 void subsample(RGB rgb[8][16], short cb[8][8], short cr[8][8])
 {
-	color red,blue,green;	
+	RGB pixel;
 	for (unsigned r = 0; r < 8; r++)
 	for (unsigned c = 0; c < 8; c++)
 	{
-		red = (getRed(rgb[r][2*c])+getRed(rgb[r][2*c+1]))/2;
-		green = (getGreen(rgb[r][2*c])+getGreen(rgb[r][2*c+1]))/2;
-		blue = (getBlue(rgb[r][2*c])+getBlue(rgb[r][2*c+1]))/2;
-		cb[r][c] = (short)RGB2Cb( red, green, blue )-128;
-		cr[r][c] = (short)RGB2Cr( red, green, blue )-128;
+		pixel.Red = (rgb[r][2*c].Red+rgb[r][2*c+1].Red)/2;
+		pixel.Green = (rgb[r][2*c].Green+rgb[r][2*c+1].Green)/2;
+		pixel.Blue = (rgb[r][2*c].Blue+rgb[r][2*c+1].Blue)/2;
+		cb[r][c] = (short)RGB2Cb( pixel.Red, pixel.Green, pixel.Blue )-128;
+		cr[r][c] = (short)RGB2Cr( pixel.Red, pixel.Green, pixel.Blue )-128;
 	}
 }
 
@@ -90,14 +90,14 @@ int main (int argc, char *argv[])
 			for (unsigned r = 0; r < 8; r++)
 			for (unsigned c = 0; c < 8; c++)
 			{
-				color R = getRed(RGB8x16[r][c]);
-				color G = getGreen(RGB8x16[r][c]);
-				color B = getBlue(RGB8x16[r][c]);
+				color R = RGB8x16[r][c].Red;
+				color G = RGB8x16[r][c].Green;
+				color B = RGB8x16[r][c].Blue;
 				// converting RGB into Y (luminance)
 				Y8x8[0][r][c] = RGB2Y(R, G, B)-128;
-				R = getRed(RGB8x16[r][c+8]);
-				G = getGreen(RGB8x16[r][c+8]);
-				B = getBlue(RGB8x16[r][c+8]);
+				R = RGB8x16[r][c+8].Red;
+				G = RGB8x16[r][c+8].Green;
+				B = RGB8x16[r][c+8].Blue;
 				// converting RGB into Y (luminance)
 				Y8x8[1][r][c] = RGB2Y(R, G, B)-128;
 			}

jpegant/jpegant-stripped/src/encoder/rgbdata.c
@@ -52,19 +52,3 @@ bool load_rgb( char *filename, RGB **mmap_addr )
 
 	return true;
 }
-
-color getRed( RGB values ){
-  color red = (values.A & 0xF8);
-  return red;
-}
-
-color getBlue( RGB values ){
-  color blue = (values.B & 0x1F)<<3;
-  return blue;
-}
-
-color getGreen( RGB values ){
-  color green = (values.A & 0x07) << 5;
-  green |= (values.B & 0xE0) >> 5;
-  return green;
-}

jpegant/jpegant-stripped/src/encoder/rgbdata.h
@@ -35,8 +35,9 @@ typedef unsigned char color;
 
 
 typedef struct {
-        color A;
-        color B;
+	color Red;
+	color Green;
+	color Blue;
 } RGB;
 
 /*
@@ -45,9 +46,6 @@ typedef struct {
 
 #ifdef __X86
 
-color getRed( RGB values );
-color getBlue( RGB values );
-color getGreen( RGB values );
 /*
  * Load the specified filename into a memory map, pointed at by mmap_addr
  *