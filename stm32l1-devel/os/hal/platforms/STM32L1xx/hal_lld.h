e1722bb565b9c3c1fb879d85d0ad0d899e08c80b
Mitch Davis <mitchd@vt.edu>
2013-05-06 14:25:38 -0400

    Enabled GPIOF in hal_lld

--- stm32l1-devel/os/hal/platforms/STM32L1xx/hal_lld.h
+++ stm32l1-devel/os/hal/platforms/STM32L1xx/hal_lld.h
@@ -214,7 +214,7 @@
 #define STM32_HAS_GPIOC         TRUE
 #define STM32_HAS_GPIOD         TRUE
 #define STM32_HAS_GPIOE         TRUE
-#define STM32_HAS_GPIOF         FALSE
+#define STM32_HAS_GPIOF         TRUE
 #define STM32_HAS_GPIOG         FALSE
 #define STM32_HAS_GPIOH         TRUE
 #define STM32_HAS_GPIOI         FALSE
